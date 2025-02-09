aea3e56d135910b72704d03c657b0b7b268478ba
bill donner <billdonner@gmail.com>
2025-02-09 09:25:26 -0600

    Initial Commit

---
+++ trivia1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  trivia1
+//
+//  Created by bill donner on 2/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ trivia1/trivia1App.swift
@@ -0,0 +1,17 @@
+//
+//  trivia1App.swift
+//  trivia1
+//
+//  Created by bill donner on 2/9/25.
+//
+
+import SwiftUI
+
+@main
+struct trivia1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
